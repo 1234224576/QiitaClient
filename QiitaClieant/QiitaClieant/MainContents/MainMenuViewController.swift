b07b2a30c5409fa9e0a6edcff63ea4215297331d
曽和修平 <sowashuhei@gmail.com>
2015-07-25 01:02:24 +0900

    WebViewControllerとMenuViewControllerの外枠を作成

---
+++ QiitaClieant/QiitaClieant/MainContents/MainMenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainMenuViewController.swift
+//  QiitaClieant
+//
+//  Created by 曽和修平 on 2015/07/25.
+//  Copyright (c) 2015年 曽和修平. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
